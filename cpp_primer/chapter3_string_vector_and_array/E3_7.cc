c15024ffa7987716231736b3b2078eb8b8a35dcb
zsq <yanbingjian1995@163.com>
2021-12-07 15:50:00 +0000

    add cpp primer chapter3 string_vector_and_array

--- cpp_primer/chapter3_string_vector_and_array/E3_7.cc
+++ cpp_primer/chapter3_string_vector_and_array/E3_7.cc
@@ -3,7 +3,7 @@
 
 int main() {
     std::string s = "123456789";
-    for (char& c: s) {  // auto推断出来的就是char
+    for (char& c: s) {  // auto infer char
         c = 'X';
     }
     std::cout << s << std::endl;
